d9dcd8d701b7a68316016ada8f43fd2ca311e4eb
Y. WATANABE <62804262+HIDAYAT-Lee@users.noreply.github.com>
2023-03-03 16:27:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+
 from GUI import *
 import sys
 
 if __name__ == "__main__":
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
