d1f3fd682a926e34bb2ddb1932ca85962bb33381
Y. WATANABE <62804262+HIDAYAT-Lee@users.noreply.github.com>
2023-03-03 16:06:54 +0800

    Add files via upload

---
+++ MachineURL.py
@@ -0,0 +1,20 @@
+D19_ZhaoJi_Floor_1 = 'http://app.littleswan.com/u_download.html?type=Ujing& uuid=0000000000000A0007555201809040059546'
+D19_ZhaoJi_Floor_2 = 'http://weixin.qq.com/r/Ej8rM5LE1M-rrdZQ92oAl?uuid=0000000000000A0007555201906010104247'
+D19_ZhaoJi_Floor_3 = 'https://q.ujing.com.cn/ucqrc/index.html?cd=0000000000000A0007555202104170176727'
+D19_ZhaoJi_Floor_4 = 'http://app.littleswan.com/u_download.html?type=Ujing&uuid=0000000000000A0007604202011190001038'
+D19_ZhaoJi_Floor_5 = 'http://weixin.qq.com/r/Ej8rM5LE1M-rrdZQ92oAl?uuid=0000000000000A0007555201901200085712'
+D19_ZhaoJi_Floor_6 = 'https://q.ujing.com.cn/ucqrc/index.html?cd=0000000000000A0007555202104170176797'
+D19_ChangGong_Floor_1 = 'http://app.littleswan.com/u_download.html?type=Ujing&uuid=0000000000000A0007604202011190000976'
+D19_ChangGong_Floor_2 = 'http://app.littleswan.com/u_download.html?type=Ujing&uuid=0000000000000A0007604202011190001378'
+D19_ChangGong_Floor_3 = 'https://q.ujing.com.cn/ucqrc/index.html?cd=0000000000000A0007555202104170175473'
+D19_ChangGong_Floor_4 = 'https://q.ujing.com.cn/ucqrc/index.html?cd=0000000000000A0007555202104170176796'
+D19_ChangGong_Floor_5 = 'http://app.littleswan.com/u_download.html?type=Ujing& uuid=0000000000000A0007555202101100166240'
+D19_ChangGong_Floor_6 = 'http://app.littleswan.com/u_download.html?type=Ujing& uuid=0000000000000A0007555202102270169223'
+
+D19_ZhaoJi = [D19_ZhaoJi_Floor_1, D19_ZhaoJi_Floor_2, D19_ZhaoJi_Floor_3, D19_ZhaoJi_Floor_4, D19_ZhaoJi_Floor_5,
+              D19_ZhaoJi_Floor_6]
+D19_ChangGong = [D19_ChangGong_Floor_1, D19_ChangGong_Floor_2, D19_ChangGong_Floor_3, D19_ChangGong_Floor_4,
+                 D19_ChangGong_Floor_5, D19_ChangGong_Floor_6]
+
+D19= [D19_ZhaoJi, D19_ChangGong]
+
---
+++ StatusFetcher.py
@@ -0,0 +1,45 @@
+import json
+
+import requests
+
+from MachineURL import *
+
+
+def getStatus(postData):
+    url = "https://phoenix.ujing.online/api/v1/wechat/devices/scanWasherCode"
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBVc2VySWQiOiJvZ3lSVDF1M0dlZU9OV2N5SGdHekZYM3RoLVVNIiwiZXhwIjoxNjg1ODAxOTEyLCJpYXQiOjE2Nzc3NjY3MTIsImlkIjoxNjE3NDk5NywibmFtZSI6IjE3Njg4MDgxODM0In0.BntJy98sKGKyzfZpxAZUg4iySWCudgb00SZefP4tmxQ",
+        "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/json; charset=utf-8",
+        "x-app-code": "BCI",
+        "Origin": "https://wx.zhinengxiyifang.cn",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.33(0x18002121) NetType/WIFI Language/zh_CN",
+        "Referer": "https://wx.zhinengxiyifang.cn/",
+        "x-app-version": "0.1.40",
+        "Connection": "keep-alive",
+    }
+
+    data = {"qrCode": postData}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    responseText = response.text
+
+    response_dict = json.loads(responseText)
+    canCreate = response_dict['data']['result']['createOrderEnabled']
+    reason = response_dict['data']['result']['reason']
+    return [canCreate, reason]
+
+
+def returnStatus(urlList):
+    result=''
+    for i in range(len(urlList)):
+        result+='第 '+str(i+1)+' 层洗衣机状态：'
+        status=getStatus(urlList[i])
+        if status[0]:
+            result+='可以洗衣'+'\n\n'
+        else:
+            result+='不可以洗衣，原因：'+status[1]+'\n\n'
+    return result
